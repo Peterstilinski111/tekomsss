e327bf50ede97513900c510ce6b83509eb839f40
Peterstilinski111 <130774452+Peterstilinski111@users.noreply.github.com>
2025-05-19 10:23:51 +0000

    sss

---
+++ ionos-1/_next/static/chunks/app/layout-b65669efd0b805f0.js
@@ -0,0 +1,23 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[185], {
+  7895: function (e, n, s) {
+    Promise.resolve().then(s.t.bind(s, 3222, 23)),
+      Promise.resolve().then(s.t.bind(s, 2471, 23)),
+      Promise.resolve().then(s.t.bind(s, 2877, 23));
+  },
+  2471: function () { },
+  2877: function (e) {
+    e.exports = {
+      style: {
+        fontFamily: "'__Overpass_432075', '__Overpass_Fallback_432075'",
+        fontStyle: "normal"
+      },
+      className: "__className_432075"
+    };
+  }
+}, function (e) {
+  e.O(0, [222, 971, 596, 744], function () {
+    return e(e.s = 7895);
+  }),
+    _N_E = e.O();
+}
+]);
